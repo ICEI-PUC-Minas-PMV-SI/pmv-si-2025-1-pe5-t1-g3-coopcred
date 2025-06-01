83f0669a017009b989f487787c9bb4b718f76fd5
PedroHenriqueNunes_ <127517961+PedroHNunesAlves@users.noreply.github.com>
2025-06-01 15:25:30 -0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -11,12 +11,19 @@ CREATE TABLE contaBancaria (
   numeroConta VARCHAR(20) UNIQUE NOT NULL,
   saldo DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
   tipoConta VARCHAR(20) NOT NULL DEFAULT 'Corrente',
-  agencia VARCHAR(20) NOT NULL,
-  );
+  agencia VARCHAR(20) NOT NULL
+);
 
 CREATE TABLE transacoes (
   id SERIAL PRIMARY KEY,
   tipoTransacao VARCHAR(20) NOT NULL,
   valor DECIMAL(10, 2) NOT NULL,
-  data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-}
+  data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuarios (username, email, password)
+VALUES (
+  'adminNovo',
+  'admin@coopcred.com',
+  '$2b$10$X8Gg5K5jkZNR5XurbiHpD.MiNByy5HFdhXDYbTxIqtB3KmkMs7Mly'
+);
